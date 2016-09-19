f040d3ed41cd9a76e7704872a1c1055142d6b6f3
aman <amanbolat@me.com>
2016-09-19 22:05:39 +0800

    Initial Commit

---
+++ JustActive/JustActive.h
@@ -0,0 +1,19 @@
+//
+//  JustActive.h
+//  JustActive
+//
+//  Created by AMANBOLAT BALABEKOV on 19.09.16.
+//  Copyright © 2016 AMANBOLAT BALABEKOV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JustActive.
+FOUNDATION_EXPORT double JustActiveVersionNumber;
+
+//! Project version string for JustActive.
+FOUNDATION_EXPORT const unsigned char JustActiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JustActive/PublicHeader.h>
+
+
